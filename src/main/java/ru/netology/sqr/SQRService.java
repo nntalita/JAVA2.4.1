e18233999c6ce3dc829c201dc0b57e4fea2cba0a
Natalya Pletneva <nntalita@gmail.com>
2021-09-27 21:20:52 +0300

    correction

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,12 +3,15 @@ package ru.netology.sqr;
 public class SQRService {
     public int sqrCalculate(int upperLimit, int lowerLimit) {
         int counter = 0;
-        for (int i = 10; i <= 99; i++)
-            if (i * i >= upperLimit)
-                if (i * i <= lowerLimit) {
+        for (int i = 10; i <= 99; i++) {
+            int sqrI = i * i;
+            if (sqrI >= upperLimit) {
+                if (sqrI <= lowerLimit) {
                     counter = counter + 1;
 
                 }
+            }
+        }
         return counter;
     }
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,10 +1,9 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.*;
+q
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
 
